e2b1fc8e8382bc9ff1b1883307f7f53f5b08dfbe
benkhalife <nvioir@gmail.com>
2025-12-24 08:15:14 -0900

    improve vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,30 @@
+import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+
+const packageVersion = JSON.parse(
+  readFileSync('package.json', 'utf-8')
+).version;
+
 export default defineConfig({
   build: {
-    // 1. Specifies that the output should be placed in the 'dist' folder.
-    outDir: 'dist',
-   
-    // 2. Use Terser for maximum compression
-    // 'terser' is used instead of 'esbuild' for Minification.
+    outDir: `dist/${packageVersion}`,
     minify: 'terser',
-    // 3. Terser settings for aggressive compression
     terserOptions: {
       compress: {
-        // Remove all console.* statements
         drop_console: true,
-        // Remove all debugger statements
         drop_debugger: true,
-        // Further optimization to remove unreachable code (e.g., in if/else)
-        // This can slightly increase Build time, but gives a smaller output.
-        // passes: 3,
       },
       format: {
-        // Remove comments from the output
         comments: false,
       },
-      // Remove all unnecessary variable and function names if possible
       mangle: true,
     },
-   
-    // 4. Library Mode settings
     lib: {
       entry: resolve(__dirname, 'src/zog.js'),
       name: 'ZogLibrary',
-      fileName: (format) => `zog.${format}.js`
+      formats: ['es'], // فقط ES format
+      fileName: () => 'zog.js'
     },
   },
 });
